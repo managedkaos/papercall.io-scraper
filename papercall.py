7e0303d1671f021aa24a955e8cc38afac38467b0
Michael Jenkins <michael.jenkins@disney.com>
2017-07-22 16:13:03 -0700

    Update requirements and link logic

--- papercall.py
+++ papercall.py
@@ -1,2 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
+
+# set the main URL
+base_url = "https://www.papercall.io"
+cfps_url = "/cfps?page=1"
+
+# make a request to the main menu URL
+r = requests.get(base_url+cfps_url)
+
+# convert the response text to soup
+soup = BeautifulSoup(r.text, "lxml")
+
+next_url = soup.find("a", {"rel":"next"})
+print next_url

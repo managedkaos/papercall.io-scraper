9f9481aee6cb0cfabb3d36f383294a59f021230f
Michael Jenkins <michael.jenkins@disney.com>
2017-07-23 09:21:02 -0700

    Complete looping over all pages

--- papercall.py
+++ papercall.py
@@ -5,11 +5,22 @@ from bs4 import BeautifulSoup
 base_url = "https://www.papercall.io"
 cfps_url = "/cfps?page=1"
 
-# make a request to the main menu URL
-r = requests.get(base_url+cfps_url)
+# loop over the pages
+while cfps_url is not None:
+    # make a request to the main menu URL
+    r = requests.get(str(base_url)+str(cfps_url))
 
-# convert the response text to soup
-soup = BeautifulSoup(r.text, "lxml")
+    # convert the response text to soup
+    soup = BeautifulSoup(r.text, "lxml")
 
-next_url = soup.find("a", {"rel":"next"})
-print next_url['href']
+    # scraping goes here
+
+    # get the next url
+    next_url = soup.find("a", {"rel":"next"})
+    
+    # 
+    try:
+        cfps_url = next_url['href']
+    except:
+        cfps_url = None
+    print cfps_url

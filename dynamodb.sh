ca9ae1d20e8d0f14032a448517103ccacfa6c900
Michael Jenkins <michael.jenkins@disney.com>
2017-08-31 10:02:33 -0700

    Update logic for stopping

--- dynamodb.sh
+++ dynamodb.sh
@@ -4,9 +4,16 @@ pid=$(echo $cmd | awk '{print $1}')
 case "$1" in
     star*)
         nohup java -Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar -sharedDb > /dev/null &
+        echo "Starting ..."
         ;;
     sto*)
-        kill $pid
+        if [[ $pid ]];
+        then
+            echo "Stopping ${pid}..."
+            kill $pid
+        else
+            echo "No process found.  Nothing to stop."
+        fi
         ;;
     stat*)
         echo $cmd        
